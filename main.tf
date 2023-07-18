90c269216f5368c28f9fbc74c7838a6a002e5998
Sonny <theli@outlook.com>
2023-07-18 11:03:51 -0700

    fix: handle single tenant, same prefix (#2)

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "azurerm_user_assigned_identity" "stacklet_identity" {
 }
 
 resource "azuread_application" "stacklet_application" {
+  count = var.azuread_application == null ? 1 : 0
   display_name    = "${var.prefix}-application"
   identifier_uris = [local.audience]
   owners = [
@@ -45,8 +46,14 @@ resource "azuread_application" "stacklet_application" {
   }
 }
 
+data "azuread_application" "stacklet_application" {
+  count = var.azuread_application == null ? 0 : 1
+  display_name = var.azuread_application
+}
+
 resource "azuread_service_principal" "stacklet_sp" {
-  application_id               = azuread_application.stacklet_application.application_id
+  count = var.azuread_application == null ? 1 : 0
+  application_id               = local.azuread_application.application_id
   app_role_assignment_required = true
   owners = [
     data.azuread_client_config.current.object_id,
@@ -57,8 +64,13 @@ resource "azuread_service_principal" "stacklet_sp" {
   }
 }
 
+data "azuread_service_principal" "stacklet_sp" {
+  count = var.azuread_application == null ? 0 : 1
+  display_name = var.azuread_application
+}
+
 resource "null_resource" "stacklet" {
-  depends_on = [azuread_application.stacklet_application, azuread_service_principal.stacklet_sp]
+  depends_on = [local.azuread_application, local.azuread_service_principal]
   provisioner "local-exec" {
     command = <<EOF
       az rest \
