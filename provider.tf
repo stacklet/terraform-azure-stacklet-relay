a84612eb9a172e46942a2c92b928048480d83064
Tim Penhey <tim@stacklet.io>
2025-07-03 15:20:47 +1200

    Be explicit about the subscription id.

--- provider.tf
+++ provider.tf
@@ -17,6 +17,17 @@
 # Note: Unlike AWS provider, Azure provider (azurerm) does not support
 # default_tags configuration. We use local.tags instead to achieve
 # consistent tagging across all resources.
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.35.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
 }
