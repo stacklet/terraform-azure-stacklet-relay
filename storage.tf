cf384305771c78df7d198d495b05cfb4d3a3528c
Cory Johns <cory@stacklet.io>
2025-12-11 11:01:34 -0500

    Use a staged approach to make storage endpoint public until function code is uploaded then make it private

--- storage.tf
+++ storage.tf
@@ -34,14 +34,14 @@ resource "azurerm_storage_account" "stacklet" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  # Disable public network access - only private endpoints allowed
-  public_network_access_enabled = false
-
-  # Network rules to control access
+  # Enable public network access temporarily to allow the function app to upload the function code.
+  # Note: This will always trigger a change on update because the azapi_update_resource will set it to false
+  # after the function app is deployed, but we need to set it to true again temporarily to allow the function
+  # app to upload any new function code.
+  public_network_access_enabled = true
   network_rules {
-    default_action = "Deny"
-    # Allow Azure services (like Function Apps) to access
-    bypass = ["AzureServices"]
+    default_action = "Allow"
+    bypass = null
   }
 
   tags = local.tags
@@ -61,4 +61,36 @@ resource "azapi_resource" "stacklet_queue" {
   }
 
   depends_on = [azurerm_storage_account.stacklet]
+}
+
+# Update storage account network settings to make it private after function app is deployed.
+# This ensures the function app code can be uploaded to the storage account before we lock it
+# down, and then the actual function run can still occur due to the bypass setting.
+resource "azapi_update_resource" "stacklet_storage_network" {
+  type        = "Microsoft.Storage/storageAccounts@2023-01-01"
+  resource_id = azurerm_storage_account.stacklet.id
+
+  body = {
+    properties = {
+      # Disable public network access - only private endpoints allowed
+      publicNetworkAccess = "Disabled"
+      # Network rules to control access
+      networkAcls = {
+        defaultAction = "Deny"
+        # Allow Azure services (like Function Apps) to access
+        bypass = "AzureServices"
+      }
+    }
+  }
+
+  depends_on = [azurerm_linux_function_app.stacklet]
+
+  # Ensure that the update is applied if the public network access is enabled again (which will
+  # happen on every update because the azurerm_storage_account resource will always make it
+  # public to allow the function app to upload any new function code).
+  lifecycle {
+    replace_triggered_by = [
+      azurerm_storage_account.stacklet.public_network_access_enabled
+    ]
+  }
 }
